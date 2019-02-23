2f656b57ff35c08dcefa0ec4332eb2e17769ec0b
w ww <956826374@qq.com>
2019-02-23 10:42:00 +0800

    add: 完成基本的架构组织

---
+++ todo-app/src/component/List.js
@@ -0,0 +1,4 @@
+import React from 'react'
+export default () => {
+  return <div>我是个任务列表</div>
+}
---
+++ todo-app/src/view/Create.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Create extends Component {
+  render(){
+    return (
+      <div>
+        用我来创建任务吧
+      </div>
+    )
+  }
+}
+export default Create
